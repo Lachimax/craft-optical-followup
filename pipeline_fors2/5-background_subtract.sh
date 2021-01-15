bb4ae4d4eb3493d52467ce95c981f677e37349db
Lachlan <lachlan.marnoch@students.mq.edu.au>
2021-01-15 11:06:40 +1100

    Deleted commented lines, no longer needed

--- pipeline_fors2/5-background_subtract.sh
+++ pipeline_fors2/5-background_subtract.sh
@@ -33,38 +33,7 @@ if ! proj_dir=$(jq -r .proj_dir ${config_file}); then
   exit
 fi
 
-param_dir=$(jq -r .param_dir "${config_file}")
-
 data_dir=$(jq -r .data_dir "${proj_dir}/epochs_fors2/${param_file}.json")
 data_title=${param_file}
-
-#if ${do_sextractor} ; then
-#    python3 ${proj_dir}//pipeline_fors2/5-background_subtract.py --directory ${data_dir} -op ${data_title} --sextractor_directory ${data_dir}/analysis/sextractor/individuals_back_subtracted/
-#
-#    mkdir ${data_dir}/analysis/sextractor/individuals_back_subtracted/
-#    if cd ${data_dir}/analysis/sextractor/individuals_back_subtracted/ ; then
-#        for fil in $(ls -d **/) ; do
-#            if cd ${fil} ; then
-#                if cp ${proj_dir}/param/sextractor/default/* . ; then
-#                    for sextract in $(ls sextract*.sh) ; do
-#                        chmod u+x ${sextract}
-#                        ./${sextract}
-#                    done
-#                    mkdir ${data_dir}/analysis/sextractor/ind_selected/${fil}
-#                    cp aperture_${aperture_diam}/* ${data_dir}/analysis/sextractor/individuals_back_subtracted/${fil}
-#                fi
-#                cd ..
-#            fi
-#        done
-#    else exit
-#    fi
-#    cp ${data_dir}/3-trimmed_with_python/${data_title}.log ${data_dir}/analysis/sextractor/individuals_back_subtracted/
-#    date +%Y-%m-%dT%T >> ${data_dir}/analysis/sextractor/individuals_back_subtracted/${data_title}.log
-#    echo Sextracted >> ${data_dir}/analysis/sextractor/individuals_back_subtracted/${data_title}.log
-#    echo "All done."
-#
-#else
-#    python3 ${proj_dir}//pipeline_fors2/5-background_subtract.py --directory ${data_dir} -op ${data_title}
-#fi
 
 python3 "${proj_dir}/pipeline_fors2/5-background_subtract.py" --directory "${data_dir}" --op "${data_title}" --origin "${origin}" --destination "${destination}"
