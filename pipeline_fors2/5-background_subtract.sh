d481f03287bfa496bb75e5fb74bdd7de7e35eb65
Lachlan <lachlan.marnoch@students.mq.edu.au>
2021-01-15 15:49:52 +1100

    Updated file to current standard

--- pipeline_fors2/5-background_subtract.sh
+++ pipeline_fors2/5-background_subtract.sh
@@ -14,17 +14,11 @@ if [[ -z ${destination} ]]; then
     destination=5-background_subtracted_with_python/
 fi
 
-normalise=$4
-if [[ -z ${normalise} ]]; then
-    normalise=true
-fi
-
 echo
 echo "Executing bash script pipeline_fors2/5-background_subtract.sh, with:"
 echo "   epoch ${param_file}"
 echo "   origin directory ${origin}"
 echo "   destination directory ${destination}"
-echo "   normalise ${normalise}"
 echo
 
 config_file="param/config.json"
@@ -33,7 +27,9 @@ if ! proj_dir=$(jq -r .proj_dir ${config_file}); then
   exit
 fi
 
-data_dir=$(jq -r .data_dir "${proj_dir}/epochs_fors2/${param_file}.json")
+param_dir=$(jq -r .param_dir "${config_file}")
+
+data_dir=$(jq -r .data_dir "${param_dir}/epochs_fors2/${param_file}.json")
 data_title=${param_file}
 
 python3 "${proj_dir}/pipeline_fors2/5-background_subtract.py" --directory "${data_dir}" --op "${data_title}" --origin "${origin}" --destination "${destination}"
