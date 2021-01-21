0995308600bdad0274479d321944e89c93d1700f
Lachlan <lachlan.marnoch@students.mq.edu.au>
2021-01-21 11:26:26 +1100

    Added setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 craft-optical-followup
 
+Pipelines and other tools for conducting optical followup of fast radio bursts.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from shutil import copy
+from os import getcwd
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="craft-optical-followup",
+    version="0.9.0",
+    author="Lachlan Marnoch",
+    short_description=long_description,
+    long_description=long_description,
+    url="https://github.com/Lachimax/craft-optical-followup",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6'
+)
+
+copy("param/config_template.yaml", "param/config.yaml")
+
+input("A fresh config.yaml file has been created in the param/ directory. Please ensure top_data_dir points to "
+      "the valid path in which to store all data products of this package."
+      "\nOnce you have done this, press any key to proceed.")
+
+import craftutils.params as p
+
+p.add_config_param(params={"proj_dir": getcwd() + "/"})
+
+import craftutils.utils as u
+
+data_dir = p.config["top_data_dir"]
+u.mkdir_check_nested(data_dir)
