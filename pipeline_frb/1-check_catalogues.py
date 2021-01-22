c3a7b7d9ccb6cf1f1b6d72c2af9e4a75db85c190
Lachlan <lachlan.marnoch@students.mq.edu.au>
2021-01-22 15:12:44 +1100

    Pull DES cutout

--- pipeline_frb/1-check_catalogues.py
+++ pipeline_frb/1-check_catalogues.py
@@ -1,9 +1,18 @@
 import craftutils.retrieve as r
+import craftutils.params as p
 
-def main(frb:str):
+
+def main(frb: str):
     r.update_frb_des_photometry(frb=frb)
     r.update_frb_sdss_photometry(frb=frb)
     r.update_frb_irsa_extinction(frb=frb)
+
+    r.update_frb_des_cutout(frb=frb)
+
+    outputs = p.frb_output_params(obj=frb)
+    for key in filter(lambda x: "in_" in x, outputs):
+        print(f"{key}: {outputs[key]}")
+
 
 if __name__ == '__main__':
     import argparse
