48684d44c9631c12c5ee4944569f761b7cbd81e7
Lachlan <lachlan.marnoch@students.mq.edu.au>
2021-04-15 07:59:56 +1000

    Basic groundwork for object classes

---
+++ craftutils/astronobjects.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+from astropy.coordinates import SkyCoord
+
+from craftutils import astrometry as a
+
+
+class Object:
+    def __init__(self,
+                 coords: Union[SkyCoord, str] = None):
+        self.coords = a.attempt_skycoord(coords)
+
+
+class FRB(Object):
+    def __init__(self):
