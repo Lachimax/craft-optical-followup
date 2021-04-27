31536a6053e01bf760bf8344c6f04b6746785d19
Lachlan <lachlan.marnoch@students.mq.edu.au>
2021-04-27 17:06:16 +1000

    Test pipeline initial work

--- pipeline_test/pipeline_test.py
+++ pipeline_test/pipeline_test.py
@@ -3,15 +3,35 @@
 import craftutils.astronobjects as objects
 import craftutils.observation as obs
 import craftutils.params as p
+import craftutils.utils as u
 
 config = p.config
 
 
 def main(field_name):
+    print("Refreshing parameter files from templates...")
+    p.refresh_params_all(quiet=True)
     field = obs.Field.from_params(name=field_name)
+    # If this field has no parameter file, ask to create one.
     if field is None:
-        print("No field of that name found in the param directory. Creating new .yaml file...")
-        obs.FRBField.new_yaml(name=field_name, path="/home/lachlan/Projects/PyCRAFT/param/fields")
+        param_path = f"{p.param_path}fields/"
+        old_params = p.object_params_frb(obj=field_name)
+        print()
+        if old_params is None:
+            print(f"{field_name} not found in the param directory.")
+            if u.select_yn(f"Create a new param file at '{param_path}{field_name}.yaml'?"):
+                obs.FRBField.new_yaml(name=field_name, path=param_path)
+                print(f"Template parameter file created at '{param_path}{field_name}.yaml'")
+                print("Please edit this file before proceeding.")
+            else:
+                print("Exiting.")
+                exit(0)
+        else:
+            print("Old format param file detected.")
+            if u.select_yn("Convert to new format?"):
+                obs.FRBField.convert_old_param(frb=field_name)
+
+    
 
 
 if __name__ == '__main__':
