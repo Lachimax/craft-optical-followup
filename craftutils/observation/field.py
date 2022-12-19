8f5bacf4ce8070ef11e2552185dec0ce5939060f
Lachlan <lachlan.marnoch@students.mq.edu.au>
2022-12-19 13:53:47 +1100

    Fixed frb ellipse bug

--- craftutils/observation/field.py
+++ craftutils/observation/field.py
@@ -1399,11 +1399,12 @@ class FRBField(Field):
         rotation_angle = img.extract_rotation_angle(ext=ext)
         theta = theta - rotation_angle
         img.extract_pixel_scale()
+
         if "include_img_err" in frb_kwargs:
             include_img_err = frb_kwargs.pop("include_img_err")
         if "edgecolor" not in frb_kwargs:
-            frb_kwargs["edgecolor"] = colour
-        if colour is not None:  # "edgecolor" not in frb_kwargs:
+            if colour is None:
+                colour = "white"
             frb_kwargs["edgecolor"] = colour
         if "facecolor" not in frb_kwargs:
             frb_kwargs["facecolor"] = "none"
