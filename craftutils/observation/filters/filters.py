Made some fixes to AB flux calculation

craftutils/observation/filters/filters.py
@@ -6,11 +6,13 @@ from typing import Union
 import astropy.table as table
 import astropy.io.votable as votable
 import astropy.units as units
+import astropy.constants as constants
 
 import craftutils.utils as u
 import craftutils.params as p
 from craftutils.retrieve import save_svo_filter
 import craftutils.observation.instrument as instrument
+import craftutils.photometry as ph
 
 active_filters = {}
 
@@ -85,25 +87,39 @@ class Filter:
             self,
             transmission: table.QTable = None
     ):
-        zp_ab = 3631 * units.Jy
-        zp_vega = self.vega_zeropoint
         if transmission is None:
             transmission, _ = self.select_transmission_table()
         delta_mag = 2.5 * np.log10(
-            self.ab_flux(transmission=transmission) / np.trapz(
-                y=zp_vega * transmission["Transmission"],
-                x=transmission["Wavelength"]
-            )
+            self.ab_flux(transmission=transmission) /
+            self.vega_flux(transmission=transmission)
         )
         return delta_mag * units.mag
+
+    def vega_flux(
+            self,
+            transmission: table.QTable = None
+    ):
+        if transmission is None:
+            transmission, _ = self.select_transmission_table()
+        return ph.flux_from_band(
+            flux=self.vega_zeropoint,
+            transmission=transmission["Transmission"],
+            frequency=transmission["Frequency"],
+            use_quantum_factor=True
+        )
 
     def ab_flux(self, transmission: table.QTable = None):
         if transmission is None:
             transmission, _ = self.select_transmission_table()
-        return np.trapz(
-            y=3631 * units.Jy * transmission["Transmission"],
-            x=transmission["Wavelength"]
+        return ph.flux_ab(
+            transmission=transmission["Transmission"],
+            frequency=transmission["Frequency"],
         )
+
+        #     np.trapz(
+        #     y=3631 * units.Jy * transmission["Transmission"],
+        #     x=transmission["Wavelength"]
+        # )
 
     def compare_transmissions(self, other: 'Filter'):
         tbl_self, tbl_other = self.find_comparable_table(other)
@@ -269,12 +285,15 @@ class Filter:
             if path is None:
                 continue
             elif not os.path.isfile(path):
-                path = self.transmission_table_paths[table_name] = None
+                self.transmission_table_paths[table_name] = None
                 continue
             if force:
                 self.transmission_tables[table_name] = None
             if table_name not in self.transmission_tables or self.transmission_tables[table_name] is None:
-                self.transmission_tables[table_name] = table.QTable.read(path)
+                tbl = table.QTable.read(path)
+                tbl["Frequency"] = (constants.c / tbl["Wavelength"]).to(units.Hz)
+                self.transmission_tables[table_name] = tbl
+
         return self.transmission_tables
 
     def transmission_by_usefulness(self):

craftutils/photometry.py
@@ -430,7 +430,6 @@ def flux_ab(
         tbl: table.QTable = None,
         transmission: Union[np.ndarray, units.Quantity] = None,
         frequency: units.Quantity = None,
-        use_quantum_factor: bool = True
 ):
     """
     For calculating the denominator of the AB Magnitude formula for a given filter.
@@ -453,14 +452,9 @@ def flux_ab(
         )
     tbl["flux"] = np.ones(len(tbl)) * AB_zeropoint
 
-    # flux_ab = np.trapz(
-    #     y=AB_zeropoint * quantum_factor * tbl["transmission"],
-    #     x=tbl["frequency"]
-    # )
-
     return flux_from_band(
         tbl,
-        use_quantum_factor=use_quantum_factor
+        use_quantum_factor=True
     )
 
 
@@ -486,7 +480,8 @@ def flux_from_band(
         if frequency is None:
             raise ValueError(
                 "If `flux` is not a table (with frequency included as column 'frequency'), `frequency` must be provided.")
-
+        if not u.is_iterable(flux):
+            flux = flux * np.ones(len(frequency))
         flux_tbl = table.QTable(
             data={
                 "frequency": frequency,