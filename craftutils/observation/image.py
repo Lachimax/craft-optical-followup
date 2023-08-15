9035e10c59adac8ff04e9617aaa701a84967511a
Lachlan <lachlan.marnoch@students.mq.edu.au>
2023-08-15 13:52:36 +1000

    Uncommented important line

--- craftutils/observation/image.py
+++ craftutils/observation/image.py
@@ -3160,7 +3160,7 @@ class ImagingImage(Image):
             **imshow_kwargs
         )
 
-        # other_args["mapping"] = mapping
+        other_args["mapping"] = mapping
         ax.set_xlim(left, right)
         ax.set_ylim(bottom, top)
 
