23237670ea4744b4f9b6af4a7f581f1c42ded857
Lachlan <lachlan.marnoch@students.mq.edu.au>
2024-05-13 16:06:38 +1000

    Major objects refactor

---
+++ craftutils/observation/objects/__init__.py
@@ -0,0 +1,9 @@
+from .position import *
+from .objects import *
+from .star import *
+from .extragalactic import *
+from .galaxy import *
+from .transient_host import *
+from .transient import *
+from .frb import *
+
